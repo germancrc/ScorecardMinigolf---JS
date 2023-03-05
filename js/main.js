3cca679babec6197bd43e6fb1ba11176b345f8db
germancrc <70047822+germancrc@users.noreply.github.com>
2023-03-05 10:57:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,6 +48,9 @@ if (document.getElementById('scores_table')) {
   <option value="1">1</option>
   <option value="2">2</option>
   <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
 </select>
 </td>`
 				}
